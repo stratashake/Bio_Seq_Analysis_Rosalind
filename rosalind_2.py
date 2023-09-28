7be41cb12dc28a45affff4190086f9cc1e315e83
stratashake <137522887+stratashake@users.noreply.github.com>
2023-09-28 14:29:41 -0500

    Update rosalind_2.py

--- rosalind_2.py
+++ rosalind_2.py
@@ -241,97 +241,10 @@
 
 ######################################################################################################
 #Problem 34
-#Catalan Numbers and RNA Secondary Structures
+#Error Correction in Reads
 
-from Bio import SeqIO
-import itertools
-
-def revcomp(s):
-    return s.translate(str.maketrans('ACTG','TGAC'))[::-1]
-
-def compare_matches(s1, s2):
-    return sum(s1[i] != s2[i] for i in range(len(s1))) == 1
-
-rna = []
-revrna = []
-for rec in SeqIO.parse("c:/Users/Matt/downloads/rosalind_cat.txt", "fasta"):
-    rna.append(str(rec.seq))
-    revrna.append(revcomp(str(rec.seq)))
-
-matches=[]
-revmatches=[]
-hammings = []
-
-for a, b in itertools.combinations(rna, 2):
-    if a == revcomp(b):
-        revmatch = (a,b)
-        revmatches.append(revmatch)
-    if a == b:
-        match = (a)
-        matches.append(match)
-
-print(f"revmatches", revmatches) 
-
-for x in revmatches:
-    for y in x:
-        for a in rna:
-            if compare_matches(y, a) == True:
-                hammings_match = (y,a)
-                hammings.append(hammings_match)
-
-# print(f"hammings_match", hammings)
-# print(f"revmatches", revmatches)        
-# print(f"matches", matches)
-
-correct = []
-final = []
-
-for x in revmatches:
-    if y in x:
-        if y in hammings_match:
-            correct.append(y)
-
-for x in correct:
-    if x in hammings_match:
-        # Check if x is the first element
-        if hammings_match[0] == x:
-            # Reverse the tuple
-            reversed_hammings_match = hammings_match[::-1]
-        else:
-            # Keep the tuple as it is
-            reversed_hammings_match = hammings_match
-
-        final.append("->".join(reversed_hammings_match))
-
-for x in matches:
-    x = revcomp(x)
-    for a in rna:
-        if compare_matches(x,a) == True:
-            temp_match = (a,x)
-            final.append("->".join(temp_match))
-
-for x in revmatches:
-    for y in x:
-        for a in rna:
-            if compare_matches(y,a) == True:
-                temp_match = (a,y)
-                final.append("->".join(temp_match))
-                
-for x in set(final):
-    print(x)
-
-#############################################################
-# #corrected via chatgpt:
 # from Bio import SeqIO
 # import itertools
-
-# rna = []
-# for rec in SeqIO.parse("c:/Users/Matt/downloads/rosalind_cat.txt", "fasta"):
-#     rna.append(str(rec.seq))
-
-# matches = []
-# revmatches = []
-# hammings = {}
 
 # def revcomp(s):
 #     return s.translate(str.maketrans('ACTG','TGAC'))[::-1]
@@ -339,40 +252,165 @@ for x in set(final):
 # def compare_matches(s1, s2):
 #     return sum(s1[i] != s2[i] for i in range(len(s1))) == 1
 
+# rna = []
+# revrna = []
+# for rec in SeqIO.parse("c:/Users/Matt/downloads/rosalind_cat.txt", "fasta"):
+#     rna.append(str(rec.seq))
+#     revrna.append(revcomp(str(rec.seq)))
+
+# matches=[]
+# revmatches=[]
+# hammings = []
+
 # for a, b in itertools.combinations(rna, 2):
-#     if a == revcomp(b):
-#         revmatch = (a, b)
-#         revmatches.append(revmatch)
-#     elif a == b:
-#         match = (a,)
+#     if a == b:
+#         match = (a)
 #         matches.append(match)
 
-# for x in revmatches:
-#     for y in x:
-#         for a in rna:
-#             if compare_matches(y, a):
-#                 if y in hammings:
-#                     hammings[y].append(a)
-#                 else:
-#                     hammings[y] = [a]
+# # Convert both lists to sets
+# rna_set = set(rna)
+# revrna_set = set(revrna)
 
-# corrections = []
+# # Find the intersection of the two sets (i.e., the sequences that are in both)
+# revmatches = rna_set & revrna_set
+
+# # If you need the matches as a list, you can convert the set back to a list
+# revmatches = list(revmatches)
+
+# # print(f"revmatches", revmatches) 
+# # print(f"matches", matches)
 
 # for x in revmatches:
-#     if x[0] in hammings and x[1] in hammings[x[0]]:
-#         correction = f"{x[0]}->{x[1]}"
-#         corrections.append(correction)
+#     for a in rna:
+#         if compare_matches(x, a) == True:
+#             hammings_match = (x,a)
+#             hammings.append(hammings_match)
+
+# # print(f"hammings_match", hammings)
+
+# correct = []
+# final = []
+
+# for x in revmatches:
+#     if x in hammings_match:
+#         correct.append(x)
+
+# for x in correct:
+#     if x in hammings_match:
+#         # Check if x is the first element
+#         if hammings_match[0] == x:
+#             # Reverse the tuple
+#             reversed_hammings_match = hammings_match[::-1]
+#         else:
+#             # Keep the tuple as it is
+#             reversed_hammings_match = hammings_match
+
+#         final.append("->".join(reversed_hammings_match))
 
 # for x in matches:
-#     x = revcomp(x[0])
-#     if x in hammings:
-#         for a in hammings[x]:
-#             correction = f"{x}->{a}"
-#             corrections.append(correction)
+#     x = revcomp(x)
+#     for a in rna:
+#         if compare_matches(x,a) == True:
+#             temp_match = (a,x)
+#             final.append("->".join(temp_match))
 
-# for correction in set(corrections):
-#     print(correction)
+# for y in revmatches:
+#     for a in rna:
+#         if compare_matches(y,a) == True:
+#             temp_match = (a,y)
+#             final.append("->".join(temp_match))
+                
+# for x in set(final):
+#     print(x)
+
+#############################################################
+#corrected via chatgpt:
+
+# from Bio import SeqIO
+# import itertools
+
+# def revcomp(s):
+#     return s.translate(str.maketrans('ACTG','TGAC'))[::-1]
+
+# def compare_matches(s1, s2):
+#     return sum(s1[i] != s2[i] for i in range(len(s1))) == 1
+
+# rna = []
+# for rec in SeqIO.parse("c:/Users/Matt/downloads/rosalind_cat.txt", "fasta"):
+#     rna.append(str(rec.seq))
+
+# matches=[]
+# hammings = []
+
+# for a, b in itertools.combinations(rna, 2):
+#     if a == b:
+#         matches.append(a)
+
+# for x in matches:
+#     for a in rna:
+#         if compare_matches(x, a):
+#             hammings.append((x, a))
+
+# final = []
+
+# for x in hammings:
+#     # Check if x[0] is the first element
+#     if x[0] == x[0]:
+#         # Reverse the tuple
+#         reversed_hammings_match = x[::-1]
+#     else:
+#         # Keep the tuple as it is
+#         reversed_hammings_match = x
+
+#     final.append("->".join(reversed_hammings_match))
+
+# for x in matches:
+#     x = revcomp(x)
+#     for a in rna:
+#         if compare_matches(x,a):
+#             final.append("->".join((a, x)))
+
+# for x in set(final):
+#     print(x)
 
 
+######################################################################################################
+#Problem 35
+#Counting Phylogenetic Ancestors
 
+#n - 2
 
+######################################################################################################
+#Problem 36
+#k-Mer Composition
+
+# s = """CTTCGAAAGTTTGGGCCGAGTCTTACAGTCGGTCTTGAAGCAAAGTAACGAACTCCACGG
+# CCCTGACTACCGAACCAGTTGTGAGTACTCAACTGGGTGAGAGTGCAGTCCCTATTGAGT
+# TTCCGAGACTCACCGGGATTTTCGATCCAGCCTCAGTCCAGTCTTGTGGCCAACTCACCA
+# AATGACGTTGGAATATCCCTGTCTAGCTCACGCAGTACTTAGTAAGAGGTCGCTGCAGCG
+# GGGCAAGGAGATCGGAAAATGTGCTCTATATGCGACTAAAGCTCCTAACTTACACGTAGA
+# CTTGCCCGTGTTAAAAACTCGGCTCACATGCTGTCTGCGGCTGGCTGTATACAGTATCTA
+# CCTAATACCCTTCAGTTCGCCGCACAAAAGCTGGGAGTTACCGCGGAAATCACAG"""
+# s = s.replace("\n", "")
+
+from Bio import SeqIO
+
+s = ""
+for rec in SeqIO.parse("./downloads/rosalind_kmer(1).txt", "fasta"):
+    s = (rec.seq)
+
+from collections import defaultdict
+
+sdict = defaultdict(int) #makes all values 0
+
+import itertools
+x = ["A", "C", "G", "T"]
+for y in itertools.product(x, repeat=4): #cartisean product is what we want
+    sdict[("".join(y))] #allows us to create mutable and connected keys like AAAC instead of "A", "A", "A", "C"
+    
+for x in range(len(s)):
+    kmer = (s[x:x+4]) #go back 4 letter increments
+    if len(kmer) == 4: #only 4 letter increments, could just cut off 3 letters in range? Like this: for x in range(len(s) - 3):
+        sdict[kmer] += 1
+
+print(' '.join(map(str, sdict.values()))) #print with no commas or brackets, just spaces
