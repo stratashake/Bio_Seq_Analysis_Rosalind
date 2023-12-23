8b5dd1e2c7b776ea57bbdf5eb29e2fff0c5a5b2b
stratashake <137522887+stratashake@users.noreply.github.com>
2023-12-23 14:59:00 -0600

    update up to probelm 42

--- Rosalind_25_50.py
+++ Rosalind_25_50.py
@@ -545,5 +545,127 @@
 ######################################################################################################
 # #Problem 42
 # #Reversal Distance
-l = "hello world"
-item for i in range(0,len(l),len(l)/2) for item in l[i:i+len(l)/2][::-1]
+
+# #This was as far as I could get on my own, it could find a reversal distance, but not the minimum. 
+# with open("./Rosalind_files/rosalind_rear.txt") as f:
+# 	lines = [line.strip().split() for line in f.readlines() if line.strip()]
+
+# def brute_reversals(s1, s2, reversals):
+#     # Check for consecutive matching elements from the beginning
+#     match_from_start = 0
+#     while match_from_start < len(s1) and match_from_start < len(s2) and s1[match_from_start] == s2[match_from_start]:
+#         match_from_start += 1
+
+#     # Check for consecutive matching elements from the end
+#     match_from_end = 0
+#     while match_from_end < len(s1) and match_from_end < len(s2) and s1[-(match_from_end + 1)] == s2[-(match_from_end + 1)]:
+#         match_from_end += 1
+
+#     # Remove the matching elements from both ends
+#     if match_from_start > 0 or match_from_end > 0:
+#         s1 = s1[match_from_start:len(s1) - match_from_end]
+#         s2 = s2[match_from_start:len(s2) - match_from_end]
+
+#     # Special case: s1 and s2 are in reverse order
+#     if s1 == s2[::-1]:
+#         s2.reverse()
+#         reversals += 1
+#         return s1, s2, reversals
+
+#     # Handling other cases
+#     if s1[-1] == s2[-1] and s1[0] == s2[0]:
+#         s1 = s1[1:-1]
+#         s2 = s2[1:-1]
+
+#     elif s1[-1] != s2[-1] and s1[0] == s2[0]:
+#         end_index = s2.index(s1[-1])
+#         s2[end_index:] = s2[end_index:][::-1]
+#         reversals += 1
+
+#     elif s1[-1] == s2[-1] and s1[0] != s2[0]:
+#         begin_index = s2.index(s1[0])
+#         s2[:begin_index + 1] = s2[:begin_index + 1][::-1]
+#         reversals += 1
+
+#     elif s1[-1] != s2[-1] and s1[0] != s2[0]:
+#         end_index = s2.index(s1[-1])
+#         begin_index = s2.index(s1[0])
+#         s2[end_index:] = s2[end_index:][::-1]
+#         s2[:begin_index + 1] = s2[:begin_index + 1][::-1]
+#         reversals += 2
+
+#     return s1, s2, reversals
+
+# for i in range(0, len(lines), 2):
+#     s1 = list(map(int, lines[i]))
+#     s2 = list(map(int, lines[i + 1]))
+#     total_reversals = 0
+
+#     while s1 and s2 and s1 != s2:
+#         s1_before, s2_before = s1[:], s2[:]  # Make a copy of s1 and s2 before the operation
+#         s1, s2, total_reversals = brute_reversals(s1, s2, total_reversals)
+
+#         # Check if no progress is made
+#         if s1 == s1_before and s2 == s2_before:
+#             break
+
+#     print(total_reversals)
+
+#This was a script I found online, it is not my code:
+# def _get_reverse_array(s):
+#     reverse_arrays = []
+#     for i in range(len(s)-1):
+#         for j in range(i+1, len(s)):
+#             r_list = s[i:j+1]
+#             r_list.reverse()
+#             reverse_arrays.append(s[:i] + r_list + s[j+1:])
+#     return reverse_arrays
+
+# def _get_reversal_distance(s1, s2, distance):
+#     # reverse s1 to s2, and reverse s2 to s1 at same time.
+#     if s1 & s2:
+#         return distance
+#     # get the reversal array of s1.
+#     new_s1 = set()
+#     for s in s1:
+#         reverse_arrays = _get_reverse_array(list(s))
+#         for r in reverse_arrays:
+#             new_s1.add(tuple(r))
+#     # get the reversal array of s2.
+#     new_s2 = set()
+#     for s in s2:
+#         reverse_arrays = _get_reverse_array(list(s))
+#         for r in reverse_arrays:
+#             new_s2.add(tuple(r))
+#     # thus we reverse s1 and s2 at same time, so distance plus 2. 
+#     distance += 2
+#     # if s1 and the reversal array of s2 has the same array, distance substract 1.
+#     if s1 & new_s2:
+#         return distance-1
+#     # if s2 and the reversal array of s1 has the same array, distance substract 1.
+#     if s2 & new_s1:
+#         return distance-1
+#     # if reversal array of s1 and the reversal array of s2 has the same array, return distance.
+#     if new_s1 & new_s2:
+#         return distance
+    
+#     distance = _get_reversal_distance(new_s1, new_s2, distance)
+#     return distance
+
+
+# if __name__ == "__main__":
+#     # load data
+#     with open("./Rosalind_files/rosalind_rear.txt", "r") as f:
+#         lines = [line.strip().split(" ") for line in f.readlines() if line.strip()]
+#     for i in range(0, len(lines), 2):
+#         a = [l for l in lines[i]]
+#         b = [l for l in lines[i+1]]
+#         distance, s1, s2 = 0, set(), set()
+#         s1.add(tuple(a)), s2.add(tuple(b))
+#         d = _get_reversal_distance(s1, s2, distance)
+#         print(d)
+
+######################################################################################################
+# #Problem 43
+# #Matching Random Motifs
+
