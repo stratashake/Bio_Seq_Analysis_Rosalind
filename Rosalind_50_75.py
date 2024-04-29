1d632bdae7908a75eee469bfe8692fd002e3a371
stratashake <137522887+stratashake@users.noreply.github.com>
2024-04-29 14:26:49 -0500

    Completed up to problem 51

--- Rosalind_50_75.py
+++ Rosalind_50_75.py
@@ -2,6 +2,39 @@
 # #Problem 51
 # #Introduction to Set Operations
 
-u = 10
-a = {1, 2, 3, 4, 5}
-b = {2, 8, 5, 10}
+# import ast  # Importing ast which will help safely evaluate string literals as Python expressions
+# import os
+
+# # Open the file
+# with open("./Rosalind_files/rosalind_seto.txt", 'r') as file:
+#     # Read the first line for the universal set size
+#     n = int(file.readline().strip())
+#     u_set = set(range(1, n + 1))  # Create the universal set from 1 to n
+
+#     # Read the second line for the first set
+#     a = file.readline().strip()
+#     a = ast.literal_eval(a)  # Convert string representation of the set to a Python set
+    
+#     # Read the third line for the second set
+#     b = file.readline().strip()
+#     b = ast.literal_eval(b)  # Convert string representation of the set to a Python set
+
+# # u_set = set(range(1, 11))
+# # a = {1, 2, 3, 4, 5}
+# # b = {2, 8, 5, 10}
+
+# set_list =union = a | b
+# intersection = a & b
+# diff_a = a.difference(b)
+# diff_b = b.difference(a)
+# comp_a = u_set.difference(a)
+# comp_b = u_set.difference(b)
+
+# folder_path = './Rosalind_files/Seto_output'
+# # Create the folder if it doesn't already exist
+# os.makedirs(folder_path, exist_ok=True)
+# # Define the full path for the new file within the new subfolder
+# file_path = os.path.join(folder_path, 'output_seto.txt')
+
+# with open(file_path, "w") as f:
+#     print("\n".join(map(str, (union, intersection, diff_a, diff_b, u_set.difference(a), u_set.difference(b)))), file=f)
