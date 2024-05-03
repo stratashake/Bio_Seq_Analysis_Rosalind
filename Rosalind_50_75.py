31b3addd7e65dfcfe5218b4d63923301c5f1a754
stratashake <137522887+stratashake@users.noreply.github.com>
2024-05-03 10:59:37 -0500

    Solved up to problem 53

--- Rosalind_50_75.py
+++ Rosalind_50_75.py
@@ -149,3 +149,57 @@
 # print("[INFO] the reversal distance drev(π,γ): {}".format(len(a_collection)))
 # for x in a_collection:
 #     print(f"{x[0]} {x[1]}")
+
+######################################################################################################
+# #Problem 53
+# #Inferring Protein from Spectrum
+
+# import numpy as np
+
+# mslist = []
+# with open("./Rosalind_files/rosalind_spec.txt") as f: 
+#     string = f.readlines()
+#     for x in string:
+#         mslist.append(float(x))
+    
+# amino_acid_masses = {
+#     'A': 71.03711,
+#     'C': 103.00919,
+#     'D': 115.02694,
+#     'E': 129.04259,
+#     'F': 147.06841,
+#     'G': 57.02146,
+#     'H': 137.05891,
+#     'I': 113.08406,
+#     'K': 128.09496,
+#     'L': 113.08406,
+#     'M': 131.04049,
+#     'N': 114.04293,
+#     'P': 97.05276,
+#     'Q': 128.05858,
+#     'R': 156.10111,
+#     'S': 87.03203,
+#     'T': 101.04768,
+#     'V': 99.06841,
+#     'W': 186.07931,
+#     'Y': 163.06333 
+# }
+
+# protein_weights = []
+# answers = []
+
+# for x in range(len(mslist)-1):
+#     difference = (abs(mslist[x] - mslist[x+1]))
+#     r_difference = round(difference, 5)
+#     protein_weights.append(r_difference)
+
+# k = list(amino_acid_masses.keys())
+# v = np.array(list(amino_acid_masses.values()))
+
+# for x in protein_weights:
+#     dist = abs(v - x)
+#     arg = np.argmin(dist)
+#     answer = k[arg]
+#     answers.append(answer)
+
+# print("".join(map(str, answers)))
